43cb3f24a4fb8b6d8f3d75ce4a75a7805e44a5a7
usanCode <sabinerugugula@gmail.com>
2025-03-15 21:22:10 +0200

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,19 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> number1 = int(input("Enter First Number : "))
+Enter First Number : 10
+>>> number2 = int(input("Enter Second Number : "))
+Enter Second Number : 30
+>>> sum = number1 + number2
+>>> print(sum)
+40
+>>> difference = number1 - number2
+>>> print(difference)
+-20
+>>> product = number1 ^ number2
+>>> print(product)
+20
+>>> division = number1 / number2
+>>> print(division)
+0.3333333333333333
